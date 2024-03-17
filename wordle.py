0f4aee688bedfd2c0e55bd1b8bd21e89b6a9a37f
punima-s <162708600+punima-s@users.noreply.github.com>
2024-03-17 20:45:32 +0000

    First draft for wordle game

---
+++ wordle.py
@@ -0,0 +1,25 @@
+# User input their guess
+secret_word = "ERUPT"
+counter = 1
+
+while counter < 7:
+    print(f"GUESS {counter}")
+    user_guess = input("Guess the secret word: ").upper()
+    if user_guess == secret_word:
+        print("Congratulation you guessed the secret word!")
+    else:
+        user_guess_list = list(user_guess)
+        correct_letter = []
+        incorrect_letter = []
+        for letter in user_guess_list:
+            if letter in secret_word:
+                correct_letter.append(letter)
+            else:
+                incorrect_letter.append(letter)
+        print("Wrong guess.\n"
+              f"Correct letters: {correct_letter}\n"
+              f"incorrect_letters: {incorrect_letter}\n")
+        if counter == 6:
+            print("Game over, you have used up all your tries.\n"
+                  f"The secret word was {secret_word}")
+    counter += 1
